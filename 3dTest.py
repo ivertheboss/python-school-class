86a84a85d0c0b554f02cc0dc1f0e43f7419b6fec
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-24 20:00:43 -0400

    Add files via upload

--- 3dTest.py
+++ 3dTest.py
@@ -2,7 +2,9 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 800))
+w = 800
+h = 800
+screen = pygame.display.set_mode((w, h))
 pygame.display.set_caption("3D python test")
 icon = pygame.image.load('cube.png')
 pygame.display.set_icon(icon)
@@ -45,8 +47,6 @@ class cube:
         self.vert8 = gameObject()
 
     def setPositions(self):
-        if self.zPos < -340:
-            self.zPos = -340
         if not self.zTemp == self.zPos or not self.xTemp == self.xPos or not self.yTemp == self.yPos:
             self.vert4.pos.z = (400 + self.zPos + self.zScale * 50)
             self.vert3.pos.z = (400 + self.zPos + self.zScale * -50)
@@ -57,23 +57,23 @@ class cube:
             self.vert6.pos.z = (400 + self.zPos + self.zScale * 50)
             self.vert5.pos.z = (400 + self.zPos + self.zScale * -50)
 
-            self.vert4.pos.x = (400 + self.xPos * (self.vert4.pos.z / 200) + self.xScale * 50 * (self.vert4.pos.z / 200))
-            self.vert3.pos.x = (400 + self.xPos * (self.vert3.pos.z / 200) + self.xScale * 50 * (self.vert3.pos.z / 200))
-            self.vert2.pos.x = (400 + self.xPos * (self.vert2.pos.z / 200) + self.xScale * 50 * (self.vert2.pos.z / 200))
-            self.vert1.pos.x = (400 + self.xPos * (self.vert1.pos.z / 200) + self.xScale * 50 * (self.vert1.pos.z / 200))
-            self.vert8.pos.x = (400 + self.xPos * (self.vert8.pos.z / 200) + self.xScale * -50 * (self.vert8.pos.z / 200))
-            self.vert7.pos.x = (400 + self.xPos * (self.vert7.pos.z / 200) + self.xScale * -50 * (self.vert7.pos.z / 200))
-            self.vert6.pos.x = (400 + self.xPos * (self.vert6.pos.z / 200) + self.xScale * -50 * (self.vert6.pos.z / 200))
-            self.vert5.pos.x = (400 + self.xPos * (self.vert5.pos.z / 200) + self.xScale * -50 * (self.vert5.pos.z / 200))
+            self.vert4.pos.x = (w/2 + self.xPos * (self.vert4.pos.z / 400) + self.xScale * 50 * (self.vert4.pos.z / 200))
+            self.vert3.pos.x = (w/2 + self.xPos * (self.vert3.pos.z / 400) + self.xScale * 50 * (self.vert3.pos.z / 200))
+            self.vert2.pos.x = (w/2 + self.xPos * (self.vert2.pos.z / 400) + self.xScale * 50 * (self.vert2.pos.z / 200))
+            self.vert1.pos.x = (w/2 + self.xPos * (self.vert1.pos.z / 400) + self.xScale * 50 * (self.vert1.pos.z / 200))
+            self.vert8.pos.x = (w/2 + self.xPos * (self.vert8.pos.z / 400) + self.xScale * -50 * (self.vert8.pos.z / 200))
+            self.vert7.pos.x = (w/2 + self.xPos * (self.vert7.pos.z / 400) + self.xScale * -50 * (self.vert7.pos.z / 200))
+            self.vert6.pos.x = (w/2 + self.xPos * (self.vert6.pos.z / 400) + self.xScale * -50 * (self.vert6.pos.z / 200))
+            self.vert5.pos.x = (w/2 + self.xPos * (self.vert5.pos.z / 400) + self.xScale * -50 * (self.vert5.pos.z / 200))
 
-            self.vert4.pos.y = (400 + self.yPos * (self.vert4.pos.z / 200) + self.yScale * -50 * (self.vert4.pos.z / 200))
-            self.vert3.pos.y = (400 + self.yPos * (self.vert3.pos.z / 200) + self.yScale * -50 * (self.vert3.pos.z / 200))
-            self.vert2.pos.y = (400 + self.yPos * (self.vert2.pos.z / 200) + self.yScale * 50 * (self.vert2.pos.z / 200))
-            self.vert1.pos.y = (400 + self.yPos * (self.vert1.pos.z / 200) + self.yScale * 50 * (self.vert1.pos.z / 200))
-            self.vert8.pos.y = (400 + self.yPos * (self.vert8.pos.z / 200) + self.yScale * -50 * (self.vert8.pos.z / 200))
-            self.vert7.pos.y = (400 + self.yPos * (self.vert7.pos.z / 200) + self.yScale * -50 * (self.vert7.pos.z / 200))
-            self.vert6.pos.y = (400 + self.yPos * (self.vert6.pos.z / 200) + self.yScale * 50 * (self.vert6.pos.z / 200))
-            self.vert5.pos.y = (400 + self.yPos * (self.vert5.pos.z / 200) + self.yScale * 50 * (self.vert5.pos.z / 200))
+            self.vert4.pos.y = (h/2 + self.yPos * (self.vert4.pos.z / 400) + self.yScale * -50 * (self.vert4.pos.z / 200))
+            self.vert3.pos.y = (h/2 + self.yPos * (self.vert3.pos.z / 400) + self.yScale * -50 * (self.vert3.pos.z / 200))
+            self.vert2.pos.y = (h/2 + self.yPos * (self.vert2.pos.z / 400) + self.yScale * 50 * (self.vert2.pos.z / 200))
+            self.vert1.pos.y = (h/2 + self.yPos * (self.vert1.pos.z / 400) + self.yScale * 50 * (self.vert1.pos.z / 200))
+            self.vert8.pos.y = (h/2 + self.yPos * (self.vert8.pos.z / 400) + self.yScale * -50 * (self.vert8.pos.z / 200))
+            self.vert7.pos.y = (h/2 + self.yPos * (self.vert7.pos.z / 400) + self.yScale * -50 * (self.vert7.pos.z / 200))
+            self.vert6.pos.y = (h/2 + self.yPos * (self.vert6.pos.z / 400) + self.yScale * 50 * (self.vert6.pos.z / 200))
+            self.vert5.pos.y = (h/2 + self.yPos * (self.vert5.pos.z / 400) + self.yScale * 50 * (self.vert5.pos.z / 200))
         self.xTemp = self.xPos
         self.yTemp = self.yPos
         self.zTemp = self.zPos
@@ -96,41 +96,48 @@ class cube:
         pygame.draw.line(screen, self.colour, (self.vert3.pos.x, self.vert3.pos.y), (self.vert4.pos.x, self.vert4.pos.y))
 
     def generateVertices(self):
-        self.vert1.circle((255,255,255))
-        self.vert2.circle((255,255,255))
-        self.vert3.circle((255,255,255))
-        self.vert4.circle((255,255,255))
-        self.vert5.circle((255,255,255))
-        self.vert6.circle((255,255,255))
-        self.vert7.circle((255,255,255))
-        self.vert8.circle((255,255,255))
+        self.vert1.circle((0,255,255))
+        self.vert2.circle((255,0,255))
+        self.vert3.circle((255,255,0))
+        self.vert4.circle((0,0,255))
+        self.vert5.circle((255,0,0))
+        self.vert6.circle((100,100,100))
+        self.vert7.circle((200,200,200))
+        self.vert8.circle((50,50,50))
 
-    def initialize(self, DVRT):
+    def generateFaces(self, WFRM):
+        pygame.draw.polygon(screen, (180, 180, 180), ((self.vert8.pos.x, self.vert8.pos.y), (self.vert7.pos.x, self.vert7.pos.y), (self.vert5.pos.x, self.vert5.pos.y), (self.vert6.pos.x, self.vert6.pos.y)), WFRM)
+
+        pygame.draw.polygon(screen, (180, 180, 180), ((self.vert7.pos.x, self.vert7.pos.y), (self.vert3.pos.x, self.vert3.pos.y), (self.vert1.pos.x, self.vert1.pos.y), (self.vert5.pos.x, self.vert5.pos.y)), WFRM)
+
+        pygame.draw.polygon(screen, (180, 180, 180), ((self.vert3.pos.x, self.vert3.pos.y), (self.vert4.pos.x, self.vert4.pos.y), (self.vert2.pos.x, self.vert2.pos.y), (self.vert1.pos.x, self.vert1.pos.y)), WFRM)
+
+        pygame.draw.polygon(screen, (180, 180, 180), ((self.vert8.pos.x, self.vert8.pos.y), (self.vert7.pos.x, self.vert7.pos.y), (self.vert3.pos.x, self.vert3.pos.y), (self.vert4.pos.x, self.vert4.pos.y)), WFRM)
+
+        pygame.draw.polygon(screen, (180, 180, 180), ((self.vert5.pos.x, self.vert5.pos.y), (self.vert1.pos.x, self.vert1.pos.y), (self.vert2.pos.x, self.vert2.pos.y), (self.vert6.pos.x, self.vert6.pos.y)), WFRM)
+
+        pygame.draw.polygon(screen, (255, 255, 255), ((self.vert8.pos.x, self.vert8.pos.y), (self.vert4.pos.x, self.vert4.pos.y), (self.vert2.pos.x, self.vert2.pos.y), (self.vert6.pos.x, self.vert6.pos.y)), WFRM)
+
+
+
+    def initialize(self, DVRT, DFAC, WFRM):
         self.setPositions()
         self.drawLines()
         if DVRT:
             self.generateVertices()
-
+        if DFAC and WFRM:
+            self.generateFaces(3)
+        if DFAC and not WFRM:
+            self.generateFaces(0)
 
 myCube = cube()
-myCube2 = cube()
-myCube3 = cube()
-myCube4 = cube()
-myCube5 = cube()
-myCube2.xPos = 100
-myCube3.xPos = -100
-myCube4.xPos = 200
-myCube5.xPos = -200
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             breakpoint(0)
     screen.fill((0, 0, 0))
 
-    myCube.initialize(True)
-    myCube2.initialize(True)
-    myCube3.initialize(True)
-    myCube4.initialize(True)
-    myCube5.initialize(True)
+    myCube.initialize(True, True, False)
 
     pygame.display.update()
